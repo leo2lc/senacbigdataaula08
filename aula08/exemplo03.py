1fb6a6026b822a27d542085967019df2db6e16d2
leo2lc <leonardolucas-c@hotmail.com>
2025-12-09 21:58:00 -0300

    Criar função

---
+++ aula08/exemplo03.py
@@ -0,0 +1,14 @@
+# def saudacao(n, i):
+#     print(f'Olá {n}, você tem {i} anos')
+
+# nome = input('Informe o nome: ')
+# idade = input('Informe o nome: ')
+# saudacao(nome, idade)
+
+def soma(n1, n2):
+    total = n1 + n2
+    print(f'O total é: {total}')
+
+num1 = float(input('Digite um número: '))
+num2 = float(input('Digite um número: '))
+soma(num1, num2)
